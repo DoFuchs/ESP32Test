5b8e443aa417089c61a725a7c6828d6021c0b90f
DoFuchs <dofuchs@googlemail.com>
2020-11-29 03:02:07 +0100

    Webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,92 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #define GREENLED 13
 #define SWITCH 12
-void setup(){
 
-  Serial.begin(115200);
-  pinMode(GREENLED, OUTPUT);
-  pinMode(SWITCH,INPUT);
-  Serial.println("Start");
-}
+const char* ssid     = "74902G";
+const char* password = "02423886407422970452";
 
-void loop(){
+WiFiServer server(80);
  
-  digitalWrite(GREENLED,LOW);
-  Serial.println("The green led is off");
-  delay(1000);
-  digitalWrite(GREENLED,HIGH);
-  Serial.println("The green led is on");
-  delay(1000);
-  if(SWITCH==HIGH){
-    Serial.println("Button pressed");
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(GREENLED, OUTPUT);      // set the LED pin mode
+ 
+    delay(10);
+ 
+    // We start by connecting to a WiFi network
+ 
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+ 
+    WiFi.begin(ssid, password);
+ 
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+ 
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    server.begin();
+ 
+}
+ 
+int value = 0;
+ 
+void loop(){
+ WiFiClient client = server.available();   // listen for incoming clients
+ 
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+ 
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+ 
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 2 on.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 2 off.<br>");
+ 
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+ 
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(GREENLED, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(GREENLED, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
   }
-
 }
